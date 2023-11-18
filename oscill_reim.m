5edba933c6ea38f0ef2b0774a360687e9d5dbfcb
DESKTOP-5E7409A\Alex <Alex>
2023-11-18 20:38:00 +0300

    no message

--- oscill_reim.m
+++ oscill_reim.m
@@ -4,7 +4,11 @@ fim = imag(field);
 [reb,rec,red] = spline(Nz,ZAxis,fre);
 [imb,imc,imd] = spline(Nz,ZAxis,fim);
 S1 = @(z) seval_cmplx(z, Nz, ZAxis, fre, fim, reb, rec, red, imb, imc, imd);
-[~, pv] = ode45(@(z, p) rhsv(z, p, Delta, S1, reidx, imidx) , ZAxis , p0v);
+
+opts = odeset('RelTol',1e-8,'AbsTol',1e-10);
+
+[~, pv] = ode45(@(z, p) rhsv(z, p, Delta, S1, reidx, imidx) , ZAxis , p0v, opts);
+
 p = pv(:,reidx) + 1i*pv(:,imidx);
 end
 
